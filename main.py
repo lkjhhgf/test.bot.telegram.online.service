2a974a0e05fe8221d3ec9961e592b29bb58543fc
MacBook <macbook@MBP-MacBook.wikilink.by>
2024-11-02 16:02:47 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+
+bot = Bot('7487424620:AAGWN4wURfzbtA0ZQfVfcN2avax152sLSnQ')
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton('Открыть веб страницу', web_app=WebAppInfo(url='https://www.instagram.com/krupenkail/')))
+    await message.answer('Privet, moi dryg!', reply_markup=markup)
+
+executor.start_polling(dp)#функция которая не дают завершить работу бота
